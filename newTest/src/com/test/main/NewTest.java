3da33efda838ddd200193bfffe9355e1d872c49d
Irilia <1473663035@qq.com>
2019-04-15 21:06:36 +0800

    更新

--- newTest/src/com/test/main/NewTest.java
+++ newTest/src/com/test/main/NewTest.java
@@ -2,6 +2,22 @@ package com.test.main;
 
 public class NewTest {
     public static void main(String[] args) {
-
+        class Solution {
+            public ListNode reverseList(ListNode head) {
+                ListNode prev = null;
+                ListNode cur = head;
+                ListNode reverseHead = null;
+                while(cur != null){
+                    ListNode curNext = cur.next;
+                    if(curNext == null){
+                        reverseHead = cur;
+                    }
+                    cur.next = prev;
+                    prev = cur;
+                    cur = curNext;
+                }
+                return reverseHead;
+            }
+        }
     }
 }
