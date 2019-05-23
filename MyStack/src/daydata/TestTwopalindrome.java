b2ee0e1bfb3203cb2314ad7bdd4109dd021615ab
Irilia <1473663035@qq.com>
2019-05-23 17:59:47 +0800

    刷题

--- MyStack/src/daydata/TestTwopalindrome.java
+++ MyStack/src/daydata/TestTwopalindrome.java
@@ -1,9 +1,11 @@
+package daydata;
 
 import java.util.Scanner;
 
 public class TestTwopalindrome {
     public static boolean judge(String s){
-        int l=0,r=s.length()-1;
+        int l=0;
+        int r=s.length()-1;
         while(l<r){
             if(s.charAt(l)==s.charAt(r)){//取出单个字符
                 l++;
@@ -15,21 +17,19 @@ public class TestTwopalindrome {
         return true;
     }
     public static void main(String[] args){
-        String s1,s2;
-        Scanner in=new Scanner(System.in);
-        s1=in.nextLine();
-        s2=in.nextLine();
+        Scanner in = new Scanner(System.in);
+        String s1=in.nextLine();
+        String s2=in.nextLine();
         int t=0;
         for(int i=0;i<s1.length();i++){
-            String ss=s1.substring(0, i)+s2+s1.substring(i);//截取部分字符串
-            if(judge(ss)){
-                ++t;
+            StringBuilder ss = new StringBuilder(s1);
+            ss.insert(i,s2);
+            if(judge(ss.toString())){
+                t++;
             }
-            System.out.println(ss);
+
         }
-        if(judge(s1+s2)){
-            ++t;
-        }
+
         System.out.println(t);
     }
 }
