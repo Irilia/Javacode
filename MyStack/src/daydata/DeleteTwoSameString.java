00a46732a100953d62d78c94f0e8bbd18293ef6e
Irilia <1473663035@qq.com>
2019-05-26 23:43:26 +0800

    刷题

---
+++ MyStack/src/daydata/DeleteTwoSameString.java
@@ -0,0 +1,17 @@
+package daydata;
+
+import java.util.Scanner;
+
+public class DeleteTwoSameString {
+    public static void main(String[] args) {
+        /*Scanner in = new Scanner(System.in);
+        String str1 = in.nextLine();
+        String str2 = in.nextLine();
+        for (int i = 0; i < ; i++) {
+            
+        }*/
+    }
+    /*public static String  TestDelete(String str1,String str2) {
+        
+    }*/
+}
