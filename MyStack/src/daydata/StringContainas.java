702f3e1d9181f8ce1fbb8c365392aedcbdc8eb60
Irilia <1473663035@qq.com>
2019-06-11 20:33:24 +0800

    刷题

--- MyStack/src/daydata/StringContainas.java
+++ MyStack/src/daydata/StringContainas.java
@@ -5,9 +5,23 @@ import java.util.Scanner;
 public class StringContainas {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String
+        int n = in.nextInt();
+        String[] p = new String[n];
+        for (int i = 0; i < n; i++) {
+            p[i] = in.nextLine();
+        }
+        String str = in.nextLine();
+        StringContainas sc  = new StringContainas();
+        boolean[] con = sc.chkSubStr(p,n,str);
+        for (int i = 0; i < con.length; i++) {
+            System.out.print(con[i]+" ");
+        }
     }
     public boolean[] chkSubStr(String[] p, int n, String s) {
-
+            boolean[] con = new boolean[n];
+            for(int i = 0;i < n; i++) {
+                con[i] = s.contains(p[i]);
+            }
+            return con;
     }
 }
