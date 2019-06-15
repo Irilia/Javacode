0b8aa96fbd0217359cabeda0d3f68af5ad0e4470
Irilia <1473663035@qq.com>
2019-06-15 20:27:49 +0800

    类集

---
+++ MyStack/src/TypeUnit/ListTest.java
@@ -0,0 +1,16 @@
+package TypeUnit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListTest {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("bit");
+        list.add("hello");
+        list.add("java");
+
+        System.out.println();
+    }
+}
