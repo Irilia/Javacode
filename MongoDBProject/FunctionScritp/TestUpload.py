f7b8cba01408bbdc5dff9c1ba05d8c9d93fde79c
Irilia <1473663035@qq.com>
2019-08-02 23:14:38 +0800

    自动化测试项目

--- MongoDBProject/FunctionScritp/TestUpload.py
+++ MongoDBProject/FunctionScritp/TestUpload.py
@@ -30,12 +30,7 @@ class TestUpload(unittest.TestCase):
 __author__ = 'sunraylily'
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re,os
+import unittest,os
 class Testupload(unittest.TestCase):
     def setUp(self):
          self.driver = webdriver.Firefox()
