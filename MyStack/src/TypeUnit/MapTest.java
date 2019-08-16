63bac3fcbe3b0a78c26826c5b68e740aeb819eeb
Irilia <1473663035@qq.com>
2019-08-16 23:28:42 +0800

    map

--- MyStack/src/TypeUnit/MapTest.java
+++ MyStack/src/TypeUnit/MapTest.java
@@ -11,14 +11,21 @@ public class MapTest {
         map.put(1,"hello");
         map.put(2,"java");
         map.put(3,"hello");
-        map.put(4,"bit");
+        map.put(4,"world");
         map.put(4,"测试");
-        System.out.println(map.get(4));
+        /*System.out.println(map.get(4));//通过key值找value值
         Set<Integer> keySet = map.keySet();
         Iterator<Integer> integerIterator = keySet.iterator();
         while(integerIterator.hasNext()) {
             Integer i = integerIterator.next();
             System.out.println(i+"="+map.get(i));
+        }*/
+        //把Map变成Set
+        Set<Map.Entry<Integer,String>> set = map.entrySet();
+        Iterator<Map.Entry<Integer,String>> iterator = set.iterator();
+        while(iterator.hasNext()) {
+            Map.Entry<Integer,String> entry = iterator.next();
+            System.out.println(entry.getKey()+"="+entry.getValue());
         }
     }
 }
